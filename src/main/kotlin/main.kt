995b739e18afc2c8d51a30a34e5c36248131754d
William Southers <william@southers.id.au>
2020-12-21 19:47:31 +1100

    advent2020-9pt1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,61 +1,37 @@
 import java.io.File
 
-var accumulator = 0
-val visitedIndexes = mutableSetOf<Int>()
 fun main(args: Array<String>) {
     println("Hello AdventOfCode2020!")
 
     val fileName = "/Volumes/AsphyxiaSSD/development/advent1/src/main/resources/input.txt"
     val lines: List<String> = File(fileName).readLines()
-    val instructions = mutableMapOf<Int, String>()
 
+    val data = mutableListOf<Long>()
+    val preambleSize = 25
+    val preamble = mutableListOf<Long>()
     for ((index, line) in lines.withIndex()) {
-        instructions[index] = line
+        data.add(line.toLong())
     }
-
-    var index = 0
-    while (!visitedIndexes.contains(index) && index < instructions.size) {
-        visitedIndexes.add(index)
-        index = eval(instructions[index]!!, index)
+    for (value in data) {
+        if (preamble.size == preambleSize) {
+           if (!valid(value, preamble)) {
+               println("Invalid: $value, $preamble")
+               break
+           }
+        }
+        preamble.add(value)
+        if (preamble.size > preambleSize) {
+            preamble.removeFirst()
+        }
     }
-    if (visitedIndexes.contains(index)) {
-        println("infinite loop")
-    }
-    println(index)
-    println(accumulator)
 }
 
-fun eval(command: String, index: Int): Int {
-    val data = command.split(" ")
-    when {
-        data[0] == "nop" -> {
-            println("nop index: $index")
-            return index + 1
-        }
-        data[0] == "acc" -> {
-            val num = data[1].toCharArray()
-            if (num[0] == '+') {
-                accumulator += data[1].replace("+", "").toInt()
-            } else if (num[0] == '-') {
-                accumulator -= data[1].replace("-", "").toInt()
-            }
-            return index + 1
-        }
-        data[0] == "jmp" -> {
-            println("jmp index: $index")
-            val num = data[1].toCharArray()
-            if (num[0] == '+') {
-                return index + data[1].replace("+", "").toInt()
-            } else if (num[0] == '-') {
-                return index - data[1].replace("-", "").toInt()
-            }
-            println("no jmp :(")
-            return index
-        }
-        else -> {
-            println("can't handle that command :(")
-            return 0
+fun valid(value: Long, preamble: MutableList<Long>): Boolean {
+    for (p in preamble) {
+        if (preamble.contains(value - p)) {
+            return true
         }
     }
+    return false
 }
 
