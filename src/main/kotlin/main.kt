8414004ab46c91aafd9f7f11ba80508aff4e29d8
William Southers <william@southers.id.au>
2020-12-21 20:10:54 +1100

    advent2020-9pt2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,7 @@
 import java.io.File
 
+var min = 0L
+var max = 0L
 fun main(args: Array<String>) {
     println("Hello AdventOfCode2020!")
 
@@ -7,29 +9,39 @@ fun main(args: Array<String>) {
     val lines: List<String> = File(fileName).readLines()
 
     val data = mutableListOf<Long>()
-    val preambleSize = 25
-    val preamble = mutableListOf<Long>()
+    val invalid = 144381670L
     for ((index, line) in lines.withIndex()) {
         data.add(line.toLong())
     }
-    for (value in data) {
-        if (preamble.size == preambleSize) {
-           if (!valid(value, preamble)) {
-               println("Invalid: $value, $preamble")
-               break
-           }
-        }
-        preamble.add(value)
-        if (preamble.size > preambleSize) {
-            preamble.removeFirst()
-        }
+
+    while (!eval(invalid, data)) {
+        data.removeFirst()
     }
+    val sum = min + max
+    println("Min: $min, Max: $max")
+    println("Sum: $sum")
 }
 
-fun valid(value: Long, preamble: MutableList<Long>): Boolean {
-    for (p in preamble) {
-        if (preamble.contains(value - p)) {
+fun eval(invalid: Long, data: MutableList<Long>): Boolean {
+    var sum = 0L
+    for (v in data) {
+        if (min == 0L) {
+            min = v
+        }
+        if (v < min) {
+            min = v
+        }
+        if (v > max) {
+            max = v
+        }
+        sum += v
+        if (sum == invalid) {
             return true
+        }
+        if (sum > invalid) {
+            min = 0L
+            max = 0L
+            break
         }
     }
     return false
