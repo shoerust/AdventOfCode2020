be48bb8de95401483c4db19379c373c4d9b9cea6
William Southers <william@southers.id.au>
2020-12-21 11:47:11 +1100

    advent2020-3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,26 +1,41 @@
 import java.io.File
 
-val regex = """([0-9]+)-([0-9]+)\s([\w]+):\s([\w]+)""".toRegex()
-
 fun main(args: Array<String>) {
     println("Hello World!")
     val fileName = "/Volumes/AsphyxiaSSD/development/advent1/src/main/resources/input.txt"
     val lines: List<String> = File(fileName).readLines()
-    var validPasswords = 0
-    lines.forEach { line -> if (valid(line)) validPasswords += 1 }
-    println(validPasswords)
+    val trees: Long
+    val trees2 = evalSlope(lines, 3, 1)
+    val trees3 = evalSlope(lines, 1, 1)
+    val trees4 = evalSlope(lines, 5, 1)
+    val trees5 = evalSlope(lines, 7, 1)
+    val trees6 = evalSlope(lines, 1, 2)
+    trees = trees2 * trees3 * trees4 * trees5 * trees6
+    println(trees)
 }
 
-fun valid(data: String): Boolean {
-    val result = regex.find(data)
-    val (pos1, pos2, letter, password) = result!!.destructured
-    val letterChar = letter.toCharArray()[0]
-    val passwordArray = password.toCharArray()
-    if (passwordArray[pos1.toInt()-1] == letterChar && passwordArray[pos2.toInt()-1] != letterChar) {
-        return true;
-    }
-    if (passwordArray[pos1.toInt()-1] != letterChar && passwordArray[pos2.toInt()-1] == letterChar) {
-        return true;
+fun evalSlope(lines: List<String>, right: Int, down: Int): Long {
+    var position = 0
+    var trees = 0L
+    var downCounter = 0
+    lines.forEach { line -> run {
+        if (downCounter == 0 || downCounter % down == 0) {
+            if (position >= line.length) {
+                position -= line.length
+            }
+            if (isTree(line, position)) {
+                trees++
+            }
+            position += right
+        }
+        downCounter++
+    } }
+    return trees
+}
+
+fun isTree(line: String, pos: Int): Boolean {
+    if (line.toCharArray()[pos] == '#') {
+        return true
     }
     return false
 }
