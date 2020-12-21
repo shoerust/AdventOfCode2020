e6262c97d715a3462b703397d09b17146cc53cee
William Southers <william@southers.id.au>
2020-12-21 20:47:37 +1100

    advent2020-10pt1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,49 +1,48 @@
 import java.io.File
 
-var min = 0L
-var max = 0L
+var oneJolt = 0L
+var threeJolt = 0L
 fun main(args: Array<String>) {
     println("Hello AdventOfCode2020!")
 
     val fileName = "/Volumes/AsphyxiaSSD/development/advent1/src/main/resources/input.txt"
     val lines: List<String> = File(fileName).readLines()
-
     val data = mutableListOf<Long>()
-    val invalid = 144381670L
     for ((index, line) in lines.withIndex()) {
         data.add(line.toLong())
     }
+    data.sort()
 
-    while (!eval(invalid, data)) {
-        data.removeFirst()
-    }
-    val sum = min + max
-    println("Min: $min, Max: $max")
-    println("Sum: $sum")
-}
+    val adapters = mutableListOf<Long>()
 
-fun eval(invalid: Long, data: MutableList<Long>): Boolean {
-    var sum = 0L
-    for (v in data) {
-        if (min == 0L) {
-            min = v
-        }
-        if (v < min) {
-            min = v
-        }
-        if (v > max) {
-            max = v
-        }
-        sum += v
-        if (sum == invalid) {
-            return true
-        }
-        if (sum > invalid) {
-            min = 0L
-            max = 0L
-            break
+    for (d in data) {
+        if (adapters.isEmpty()) {
+            if (d <= 3L) {
+                if (d == 1L) {
+                    oneJolt++
+                }
+                if (d == 3L) {
+                    threeJolt++
+                }
+                adapters.add(d)
+            }
+        } else {
+            if (d - adapters.last() <= 3) {
+                if (d - adapters.last() == 1L) {
+                    oneJolt++
+                }
+                if (d - adapters.last() == 3L) {
+                    threeJolt++
+                }
+                adapters.add(d)
+            } else {
+                break
+            }
         }
     }
-    return false
+    threeJolt++
+    val result = oneJolt * threeJolt
+    println("Result: $result, One: $oneJolt, Three: $threeJolt")
+
 }
 
