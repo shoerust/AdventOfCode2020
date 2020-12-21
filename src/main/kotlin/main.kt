63fab082574b65ee549ae3b2af32b2b9bec3388c
William Southers <william@southers.id.au>
2020-12-22 10:12:58 +1100

    advent2020-10pt2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,16 +2,19 @@ import java.io.File
 
 var oneJolt = 0L
 var threeJolt = 0L
+val data = mutableListOf<Long>()
 fun main(args: Array<String>) {
     println("Hello AdventOfCode2020!")
 
     val fileName = "/Volumes/AsphyxiaSSD/development/advent1/src/main/resources/input.txt"
     val lines: List<String> = File(fileName).readLines()
-    val data = mutableListOf<Long>()
+
+    data.add(0)
     for ((index, line) in lines.withIndex()) {
         data.add(line.toLong())
     }
     data.sort()
+    data.add(data.last() + 3)
 
     val adapters = mutableListOf<Long>()
 
@@ -44,5 +47,26 @@ fun main(args: Array<String>) {
     val result = oneJolt * threeJolt
     println("Result: $result, One: $oneJolt, Three: $threeJolt")
 
+    println("Part 2 -----")
+
+   println(calculate(0))
+}
+
+var memo = mutableMapOf<Int, Long>()
+fun calculate(index: Int): Long {
+    if (index == data.size - 1) {
+        return 1
+    }
+    if (memo.containsKey(index)) {
+        return memo[index]!!
+    }
+    var count = 0L
+    for (j in index+1 until data.size) {
+        if ((data[j] - data[index]) <= 3) {
+            count += calculate(j)
+        }
+    }
+    memo[index] = count
+    return count
 }
 
