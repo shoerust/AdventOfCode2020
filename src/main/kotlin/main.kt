f1848484e619851c524bfa4c6887b4e4cac1c5bf
William Southers <william@southers.id.au>
2020-12-21 15:44:15 +1100

    advent2020-5

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,89 +1,79 @@
 import java.io.File
+import kotlin.math.abs
 
-val regex = """(.+):(.+)""".toRegex()
-val requiredKeys = setOf<String>("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
-val cmRegex = """([0-9]+)cm""".toRegex()
-val inchRegex = """([0-9]+)in""".toRegex()
-val colorRegex = """#([0-9]|[a-f]){6}""".toRegex()
-val eyeRegex = """^(amb|blu|brn|gry|grn|hzl|oth)${'$'}""".toRegex()
-val pidRegex = """[0-9]{9}""".toRegex()
 fun main(args: Array<String>) {
     println("Hello AdventOfCode2020!")
 
     val fileName = "/Volumes/AsphyxiaSSD/development/advent1/src/main/resources/input.txt"
     val lines: List<String> = File(fileName).readLines()
-    var validPassports = 0
-    var passport = ""
-    var rk = mutableSetOf<String>()
+    var maxSeatId = 0
+    var idList = mutableListOf<Int>()
     for (line in lines) {
-        if (line.isNotBlank()) {
-            passport += " $line"
+       val seatId = calculateSeatId(line)
+        idList.add(seatId)
+       if (seatId > maxSeatId) {
+           maxSeatId = seatId
+       }
+    }
+    println(maxSeatId)
+    idList.sort()
+    var previousId = -1
+    for (id in idList) {
+        previousId = if (previousId == -1) {
+            id
         } else {
-            rk = validate(passport.trim())
-            if (rk.isEmpty()) {
-                validPassports++
+            if (id == (previousId + 2)) {
+                println("Id: $id")
             }
-            passport = ""
+            id
         }
     }
-    if (rk.isEmpty()) {
-        validPassports++
-    }
-    println(validPassports)
 }
 
-fun validate(passport: String): MutableSet<String> {
 
-    val temp = mutableSetOf<String>()
-    temp.addAll(requiredKeys)
-
-    val pairs = passport.split(" ")
-    for (pair in pairs) {
-        val match = regex.find(pair)
-        val (key, value) = match!!.destructured
-        if (requiredKeys.contains(key)) {
-            if (key == "byr") {
-              if (value.toInt() in 1920..2002) {
-                  temp.remove(key)
-              }
-            } else if (key == "iyr") {
-                if (value.toInt() in 2010..2020) {
-                    temp.remove(key)
-                }
-            } else if (key == "eyr") {
-                if (value.toInt() in 2020..2030) {
-                    temp.remove(key)
-                }
-            } else if (key == "hgt") {
-                val cm = cmRegex.find(value)
-                if (cm != null) {
-                    val (number) = cm.destructured
-                    if (number.toInt() in 150..193) {
-                        temp.remove(key)
-                    }
-                } else {
-                    val inch = inchRegex.find(value)
-                    if (inch != null) {
-                        val (number) = inch.destructured
-                        if (number.toInt() in 59..76) {
-                            temp.remove(key)
-                        }
-                    }
-                }
-            } else if (key == "hcl") {
-                if (colorRegex.matches(value)) {
-                    temp.remove(key)
-                }
-            } else if (key == "ecl") {
-               if (eyeRegex.matches(value)) {
-                   temp.remove(key)
-               }
-            } else if (key == "pid") {
-              if (pidRegex.matches(value)) {
-                  temp.remove(key)
-              }
+fun calculateSeatId(line: String): Int {
+    var rowMax = 127
+    var rowMin = 0
+    var seatMax = 7
+    var seatMin = 0
+    var useRowMin = false
+    var useSeatMin = false
+    var finalRow = 0
+    var finalSeat = 0
+    for (direction in line) {
+        when (direction) {
+            'F' -> {
+                rowMax -= ((rowMax - rowMin) / 2) + 1
+                useRowMin = true
+            }
+            'B' -> {
+                rowMin += ((rowMax - rowMin) / 2) + 1
+                useRowMin = false
+            }
+            'R' -> {
+                seatMin += ((seatMax - seatMin) / 2) + 1
+                useSeatMin = false
+            }
+            'L' -> {
+                seatMax -= ((seatMax - seatMin) / 2) + 1
+                useSeatMin = true
             }
         }
     }
-    return temp
+    println(line)
+    println("$rowMax $rowMin $seatMax $seatMin")
+    finalRow = if (useRowMin) {
+        rowMin
+    } else {
+        rowMax
+    }
+    finalSeat = if (useSeatMin) {
+        seatMin
+    } else {
+        seatMax
+    }
+    println("$finalRow $finalSeat")
+    val id = finalRow * 8 + finalSeat
+    println(id)
+    return id
 }
