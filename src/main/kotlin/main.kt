a55c37af7a237d478eb2819d23bcc16c42655b58
William Southers <william@southers.id.au>
2020-12-22 19:41:30 +1100

    advent2020-13pt1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,65 +1,32 @@
 import java.io.File
-import java.lang.Math.abs
 
-val regex = """([N|S|E|W|L|R|F])([0-9]+)""".toRegex()
 fun main(args: Array<String>) {
     println("Hello AdventOfCode2020!")
 
     val fileName = "/Volumes/AsphyxiaSSD/development/advent1/src/main/resources/input.txt"
     val lines: List<String> = File(fileName).readLines()
-
+    var timestamp = 0L
     for ((index, line) in lines.withIndex()) {
-        val matchResult = regex.find(line)
-        val direction = matchResult!!.groupValues[1]
-        val value = matchResult!!.groupValues[2].toLong()
 
-        when (direction) {
-            "N" -> {
-                Waypoint.y += value
-            }
-            "S" -> {
-                Waypoint.y -= value
-            }
-            "E" -> {
-                Waypoint.x += value
-            }
-            "W" -> {
-                Waypoint.x -= value
-            }
-            "L" -> {
-                for (i in 1..value/90) {
-                    val temp = Waypoint.y
-                    Waypoint.y = Waypoint.x
-                    Waypoint.x = temp * -1
+        if (index == 0) {
+          timestamp = line.toLong()
+        } else {
+            val buses = line.split(",")
+            var shortestWait = timestamp
+            var soonestNo = -1
+            var ride = 0L
+            for (bus in buses) {
+                if (bus != "x") {
+                    val longBus = bus.toLong()
+                    val minutes = longBus - (timestamp % longBus)
+                    if (minutes < shortestWait) {
+                        shortestWait = minutes
+                        ride = longBus
+                    }
                 }
             }
-            "R" -> {
-                for (i in 1..value/90) {
-                    val temp = Waypoint.y
-                    Waypoint.y = Waypoint.x * -1
-                    Waypoint.x = temp
-                }
-            }
-            "F" -> {
-                Ship.x += Waypoint.x * value
-                Ship.y += Waypoint.y * value
-            }
-            else -> {
-                println("wtf")
-            }
+            println(shortestWait * ride)
         }
     }
-    val result = abs(Ship.x) + abs(Ship.y)
-    println("x: ${Ship.x}, y: ${Ship.y}, result: $result")
-}
-
-object Ship {
-    var x = 0L
-    var y = 0L
-}
-
-object Waypoint {
-    var x = Ship.x + 10L
-    var y = Ship.y + 1L
 }
 
