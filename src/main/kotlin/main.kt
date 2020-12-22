1e25dea9eaa41c6da3646b2a32e0b75b5bb57640
William Southers <william@southers.id.au>
2020-12-22 13:39:49 +1100

    advent2020-12pt1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,7 @@
 import java.io.File
-import java.lang.Exception
+import java.lang.Math.abs
 
-
-var plan = mutableListOf<MutableList<Char>>()
+val regex = """([N|S|E|W|L|R|F])([0-9]+)""".toRegex()
 fun main(args: Array<String>) {
     println("Hello AdventOfCode2020!")
 
@@ -10,158 +9,181 @@ fun main(args: Array<String>) {
     val lines: List<String> = File(fileName).readLines()
 
     for ((index, line) in lines.withIndex()) {
-        val listLine = mutableListOf<Char>()
-        for (c in line) {
-            listLine.add(c)
-        }
-        plan.add(listLine)
-    }
-    var iterations = 0
-    while (updateSeats()) {
-        iterations++
-    }
-    println(plan)
-    println(iterations)
-    println(countOccupied())
+        var matchResult = regex.find(line)
+        val direction = matchResult!!.groupValues[1]
+        val value = matchResult!!.groupValues[2].toLong()
 
+        when (direction) {
+            "N" -> {
+                Ship.y = Ship.y + value
+            }
+            "S" -> {
+                Ship.y = Ship.y - value
+            }
+            "E" -> {
+                Ship.x = Ship.x + value
+            }
+            "W" -> {
+                Ship.x = Ship.x - value
+            }
+            "L" -> {
+                rotateLeft(value)
+            }
+            "R" -> {
+
+                rotateRight(value)
+            }
+            "F" -> {
+                when (Ship.direction) {
+                    "N" -> {
+                        Ship.y = Ship.y + value
+                    }
+                    "S" -> {
+                        Ship.y = Ship.y - value
+                    }
+                    "E" -> {
+                        Ship.x = Ship.x + value
+                    }
+                    "W" -> {
+                        Ship.x = Ship.x - value
+                    }
+                }
+            }
+            else -> {
+                println("wtf")
+            }
+        }
+    }
+    val result = kotlin.math.abs(Ship.x) + kotlin.math.abs(Ship.y)
+    println("x: ${kotlin.math.abs(Ship.x)}, y: ${kotlin.math.abs(Ship.y)}, result: $result")
 }
 
-fun updateSeats(): Boolean {
-
-    val newPlan = mutableListOf<MutableList<Char>>()
-    for (c in plan) {
-        newPlan.add(c.toMutableList())
-    }
-    for ((indexR, a) in plan.withIndex()) {
-        for ((indexC, b) in a.withIndex()) {
-            var surroundingOccupied = 0
-            var seen = false
-            var counter = 0
-            while (!seen) {
-                counter++
-                seen = isChair(indexR, indexC + counter)
-            }
-            if (isOccupied(indexR, indexC + counter)) {
-                surroundingOccupied++
-            }
-
-            seen = false
-            counter = 0
-            while (!seen) {
-                counter++
-                seen = isChair(indexR, indexC - counter)
-            }
-            if (isOccupied(indexR, indexC - counter)) {
-                surroundingOccupied++
-            }
-
-            seen = false
-            counter = 0
-            while (!seen) {
-                counter++
-                seen = isChair(indexR + counter, indexC)
-            }
-            if (isOccupied(indexR + counter, indexC)) {
-                surroundingOccupied++
-            }
-
-            seen = false
-            counter = 0
-            while (!seen) {
-                counter++
-                seen = isChair(indexR - counter, indexC)
-            }
-            if (isOccupied(indexR - counter, indexC)) {
-                surroundingOccupied++
-            }
-
-            seen = false
-            counter = 0
-            while (!seen) {
-                counter++
-                seen = isChair(indexR - counter, indexC - counter)
-            }
-            if (isOccupied(indexR - counter, indexC - counter)) {
-                surroundingOccupied++
-            }
-
-            seen = false
-            counter = 0
-            while (!seen) {
-                counter++
-                seen = isChair(indexR + counter, indexC + counter)
-            }
-            if (isOccupied(indexR + counter, indexC + counter)) {
-                surroundingOccupied++
-            }
-
-            seen = false
-            counter = 0
-            while (!seen) {
-                counter++
-                seen = isChair(indexR - counter, indexC + counter)
-            }
-            if (isOccupied(indexR - counter, indexC + counter)) {
-                surroundingOccupied++
-            }
-
-            seen = false
-            counter = 0
-            while (!seen) {
-                counter++
-                seen = isChair(indexR + counter, indexC - counter)
-            }
-            if (isOccupied(indexR + counter, indexC - counter)) {
-                surroundingOccupied++
-            }
-
-            if (plan[indexR][indexC] == 'L') {
-                if (surroundingOccupied == 0) {
-                    newPlan[indexR][indexC] = '#'
+fun rotateLeft(value: Long) {
+    when (value) {
+        90L -> {
+            when (Ship.direction) {
+                "N" -> {
+                    Ship.direction = "W"
                 }
-            } else if (plan[indexR][indexC] == '#') {
-                if (surroundingOccupied >= 5) {
-                    newPlan[indexR][indexC] = 'L'
+                "E" -> {
+                    Ship.direction = "N"
+                }
+                "S" -> {
+                    Ship.direction = "E"
+                }
+                "W" -> {
+                    Ship.direction = "S"
+                }
+            }
+        }
+        180L -> {
+            when (Ship.direction) {
+                "N" -> {
+                    Ship.direction = "S"
+                }
+                "E" -> {
+                    Ship.direction = "W"
+                }
+                "S" -> {
+                    Ship.direction = "N"
+                }
+                "W" -> {
+                    Ship.direction = "E"
+                }
+            }
+        }
+        270L -> {
+            when (Ship.direction) {
+                "N" -> {
+                    Ship.direction = "E"
+                }
+                "E" -> {
+                    Ship.direction = "S"
+                }
+                "S" -> {
+                    Ship.direction = "W"
+                }
+                "W" -> {
+                    Ship.direction = "N"
                 }
             }
         }
     }
-    if (!plan.containsAll(newPlan)) {
-        plan = newPlan
-        return true
-    }
-    return false
 }
 
-fun isChair(r: Int, c: Int): Boolean {
-    return try {
-        plan[r][c] == 'L' || plan[r][c] == '#'
-    } catch (e: Exception) {
-        true
-    }
-}
-
-fun isOccupied(r: Int, c: Int): Boolean {
-    return try {
-        plan[r][c] == '#'
-    } catch (e: Exception) {
-        false
-    }
-}
-
-fun countOccupied(): Int {
-    var counter = 0
-    for ((indexR, a) in plan.withIndex()) {
-        for ((indexC, b) in a.withIndex()) {
-            if (b == '#') {
-                counter++
+fun rotateRight(value: Long) {
+    when (value) {
+        90L -> {
+            when (Ship.direction) {
+                "N" -> {
+                    Ship.direction = "E"
+                }
+                "E" -> {
+                    Ship.direction = "S"
+                }
+                "S" -> {
+                    Ship.direction = "W"
+                }
+                "W" -> {
+                    Ship.direction = "N"
+                }
+            }
+        }
+        180L -> {
+            when (Ship.direction) {
+                "N" -> {
+                    Ship.direction = "S"
+                }
+                "E" -> {
+                    Ship.direction = "W"
+                }
+                "S" -> {
+                    Ship.direction = "N"
+                }
+                "W" -> {
+                    Ship.direction = "E"
+                }
+            }
+        }
+        270L -> {
+            when (Ship.direction) {
+                "N" -> {
+                    Ship.direction = "W"
+                }
+                "E" -> {
+                    Ship.direction = "N"
+                }
+                "S" -> {
+                    Ship.direction = "E"
+                }
+                "W" -> {
+                    Ship.direction = "S"
+                }
             }
         }
     }
-    return counter
 }
 
-class Floor : Exception() {
+fun move(value: Long, direction: String) {
+    when (direction) {
+        "N" -> {
+            Ship.y = Ship.y + value
+        }
+        "S" -> {
+            Ship.y = Ship.y - value
+        }
+        "E" -> {
+            Ship.x = Ship.x + value
+        }
+        "W" -> {
+            Ship.x = Ship.x - value
+        }
+    }
+}
 
+object Ship {
+    var direction = "E"
+    var x = 0L
+    var y = 0L
 }
 
