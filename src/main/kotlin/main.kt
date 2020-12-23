59c03c17c728906f2f98c594fc9c3613e98bcdaf
William Southers <william@southers.id.au>
2020-12-23 11:46:59 +1100

    advent2020-15pt2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,34 +1,48 @@
 import java.io.File
 import java.util.*
 
+class Number {
+    var lastSpoken = 0L
+    var secondLastSpoken = 0L
+}
+
 fun main(args: Array<String>) {
     println("Hello AdventOfCode2020!")
 
     val fileName = "/Volumes/AsphyxiaSSD/development/advent1/src/main/resources/input.txt"
     val lines: List<String> = File(fileName).readLines()
     val spoken = mutableListOf<Long>()
+    val numCache = mutableMapOf<Long, Number>()
     for ((index, line) in lines.withIndex()) {
         val nums = line.split(",")
-        for (n in nums) {
+        for ((numIndex, n) in nums.withIndex()) {
             spoken.add(n.toLong())
+            val number = Number()
+            number.lastSpoken = numIndex + 1L
+            number.secondLastSpoken = -1L
+            numCache[n.toLong()] = number
         }
     }
 
     var curr = 0L
-    while (spoken.size < 2020) {
+
+    while (spoken.size < 30_000_000) {
         spoken.add(curr)
         var secondLastSpoken = -1L
         var lastSpoken = -1L
-        for ((index, s) in spoken.withIndex()) {
-            if (s == curr) {
-                secondLastSpoken = lastSpoken
-                lastSpoken = index + 1L
-            }
-        }
-        curr = if (secondLastSpoken == -1L) {
-            0L
-        }  else {
-            lastSpoken - secondLastSpoken
+        if (numCache.containsKey(curr)) {
+            val number = numCache[curr]!!
+            secondLastSpoken = number.lastSpoken
+            lastSpoken = spoken.size.toLong()
+            number.secondLastSpoken = secondLastSpoken
+            number.lastSpoken = lastSpoken
+            curr = lastSpoken - secondLastSpoken
+        } else {
+            val number = Number()
+            number.lastSpoken = spoken.size.toLong()
+            number.secondLastSpoken = -1L
+            numCache[curr] = number
+            curr = 0L
         }
     }
     println(spoken.last())
