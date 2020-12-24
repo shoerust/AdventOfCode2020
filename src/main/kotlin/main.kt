1cd89d8bfacb4badec530b324b0462aeb4992302
William Southers <william@southers.id.au>
2020-12-24 17:19:43 +1100

    advent2020-24pt1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,79 +5,136 @@ fun main(args: Array<String>) {
 
     val fileName = "/Volumes/AsphyxiaSSD/development/advent1/src/main/resources/input.txt"
     val lines: List<String> = File(fileName).readLines()
-    val rightCupByCup = mutableMapOf<Int, Int>()
-    val max = 1000000
-    var leftCup = -1
-    var firstCup = -1
+    val tilePaths  = mutableListOf<List<String>>()
+    val directions = mutableListOf<String>("e", "se", "sw", "w", "nw", "ne")
+    val tiles = mutableMapOf<Coord, Tile>()
     for ((index, line) in lines.withIndex()) {
-        for (c in line.toCharArray()) {
-            if (leftCup > -1) {
-                rightCupByCup[leftCup] = c.toString().toInt()
+        var path = line
+        val pathList = mutableListOf<String>()
+        while (path.isNotEmpty()) {
+            for (d in directions) {
+                if (path.startsWith(d)) {
+                    pathList.add(d)
+                   path = path.removePrefix(d)
+                }
+            }
+        }
+        tilePaths.add(pathList)
+    }
+    println(tilePaths)
+
+    for (path in tilePaths) {
+        val c = Coord()
+        for (d in path) {
+//            when (d) {
+//                "e" -> {
+//                    c.y--
+//                }
+//                "se" -> {
+//                    c.y--
+//                    c.x++
+//                }
+//                "sw" -> {
+//                    c.x--
+//                }
+//                "w" -> {
+//                    c.y++
+//                    c.x--
+//                }
+//                "nw" -> {
+//                    c.y++
+//                }
+//                "ne" -> {
+//                    c.x++
+//                }
+//            }
+            when (d) {
+                "e" -> {
+                   c.x++
+                    c.y--
+                }
+                "se" -> {
+                    c.z++
+                    c.y--
+                }
+                "sw" -> {
+                   c.x--
+                   c.z++
+                }
+                "w" -> {
+                   c.x--
+                    c.y++
+                }
+                "nw" -> {
+                   c.z--
+                    c.y++
+                }
+                "ne" -> {
+                    c.x++
+                    c.z--
+                }
+            }
+        }
+        if (tiles.containsKey(c)) {
+            val tile = tiles[c]!!
+            if (tile.colour == "white") {
+                tile.colour = "black"
             } else {
-                firstCup = c.toString().toInt()
+                tile.colour = "white"
             }
-            leftCup = c.toString().toInt()
+            tiles[c] = tile
+        } else {
+            val t = Tile()
+            t.colour = "black"
+            tiles[c] = t
         }
     }
 
-    for (i in 10..1000000) {
-        rightCupByCup[leftCup] = i
-        leftCup = i
-    }
-    rightCupByCup[leftCup] = firstCup
-
-    var move = 0
-    var currentCup = 5
-    while (move < 10000000) {
-        //println(rightCupByCup.keys)
-        //println(currentCup)
-
-            if (move % 100000 == 0) {
-               //println("Move $move")
-            }
-
-        val p1 = rightCupByCup[currentCup]!!
-        val p2 = rightCupByCup[p1]!!
-        val p3 = rightCupByCup[p2]!!
-
-        //println("$p1 $p2 $p3")
-
-        var dest = -1
-        var diff = 1
-        var candidate = currentCup - 1
-        while (dest == -1) {
-            if (candidate == 0) {
-                candidate = max
-            }
-            if (candidate != p1 && candidate != p2 && candidate != p3) {
-                dest = candidate
-            }
-            candidate--
+    var counter = 0
+    for ((c, v) in tiles) {
+        if (v.colour == "black") {
+            counter++
         }
-        //println("Dest $dest")
-        val n = rightCupByCup[dest]!!
-        val l = rightCupByCup[p3]!!
-        rightCupByCup[dest] = p1
-        rightCupByCup[p1] = p2
-        rightCupByCup[p2] = p3
-        rightCupByCup[p3] = n
-        rightCupByCup[currentCup] = l
-        currentCup = l
-        move++
     }
-    var i = 1
-    var done = false
-//    while (!done) {
-//        i = rightCupByCup[i]!!
-//        print(" ")
-//        if (i == 1) {
-//            done = true
-//        }
-//    }
-    val t = rightCupByCup[1]!!
-    val t2 = rightCupByCup[t]!!
-    val result = t.toLong() * t2.toLong()
-    println("$t $t2 $result")
-    //24987653
+    println(tiles)
+    println("black $counter")
+}
+
+class Coord {
+    var x = 0
+    var y = 0
+    var z = 0
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Coord
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+        if (z != other.z) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = x
+        result = 31 * result + y
+        result = 31 * result + z
+        return result
+    }
+
+    override fun toString(): String {
+        return "Coord(x=$x, y=$y, z=$z)"
+    }
+
+
+}
+
+class Tile {
+    var colour = "white"
+    override fun toString(): String {
+        return "Tile(colour='$colour')"
+    }
 }
 
